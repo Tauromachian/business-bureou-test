204b236ac05b0b8e086242f587a666c732668bfa
Jose Garcia <jogarcia@tuta.io>
2023-05-24 15:20:51 -0400

    Make basic footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,7 +1,51 @@
 <template>
-  <div></div>
+  <footer class="d-flex flex-column align-center">
+    <div>Kids r Us</div>
+    <div>
+      <NuxtLink
+        v-for="(link, index) in state.links"
+        :key="`link-${index}`"
+        :to="link.to"
+      >
+        {{ link.text }}
+      </NuxtLink>
+    </div>
+    <div>
+      <NuxtLink
+        v-for="(social, index) in state.socials"
+        :key="`link-${index}`"
+        :to="social.to"
+      >
+        {{ social.icon }}
+      </NuxtLink>
+    </div>
+    <div>
+      <p>
+        Privacy policy/This is a sample website - cmsmaster&copy;2022/All rights
+        reserved
+      </p>
+    </div>
+  </footer>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+
+const state = reactive({
+  links: [
+    { text: "Shop", to: "" },
+    { text: "Sale", to: "" },
+    { text: "Girls", to: "" },
+    { text: "Boys", to: "" },
+    { text: "Lookbook", to: "" },
+    { text: "Contacts", to: "" },
+  ],
+  socials: [
+    { icon: "facebook", to: "" },
+    { icon: "twitter", to: "" },
+    { icon: "instagram", to: "" },
+  ],
+});
+</script>
 
 <style lang="scss" scoped></style>
