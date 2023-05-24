6ead42de4889c32bdc35780231abc60cc2fc640a
Jose Garcia <jogarcia@tuta.io>
2023-05-24 15:24:06 -0400

    Make basic nav

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,7 +1,42 @@
 <template>
-  <div></div>
+  <header class="d-flex flex-column align-center">
+    <div>Logo</div>
+    <div class="d-flex">
+      <NuxtLink
+        v-for="(link, index) in state.links"
+        :key="`link-${index}`"
+        :to="link.to"
+      >
+        {{ link.text }}
+      </NuxtLink>
+      <NuxtLink
+        v-for="(link, index) in state.storeLinks"
+        :key="`link-${index}`"
+        :to="link.to"
+      >
+        {{ link.icon }}
+      </NuxtLink>
+    </div>
+  </header>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
 
+const state = reactive({
+  links: [
+    { text: "Home", to: "" },
+    { text: "Shope Features", to: "" },
+    { text: "Clothes", to: "" },
+    { text: "Pages", to: "" },
+    { text: "Shortcodes", to: "" },
+    { text: "Post types", to: "" },
+  ],
+  storeLinks: [
+    { icon: "heart", to: "" },
+    { icon: "cart", to: "" },
+    { icon: "search", to: "" },
+  ],
+});
+</script>
 <style lang="scss" scoped></style>
