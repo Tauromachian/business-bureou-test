dbb9c3b917658b48f944b233e11a20943c0b995b
Jose Garcia <jogarcia@tuta.io>
2023-05-24 17:24:59 -0400

    Add logo component

---
+++ components/AppLogo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="icon-holder">
+    <img src="/logo.png" alt="logo" />
+  </div>
+</template>
+
+<style scoped>
+.icon-holder {
+  text-align: center;
+  border-radius: 50%;
+  width: 18em;
+  height: 18em;
+  margin-bottom: 1em;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+</style>
