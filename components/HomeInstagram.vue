599057405eb5ddffa10757f9fe3d0ec349644baa
Jose Garcia <jogarcia@tuta.io>
2023-05-25 15:05:03 -0400

    Display instagram section

--- components/HomeInstagram.vue
+++ components/HomeInstagram.vue
@@ -1,7 +1,35 @@
 <template>
-  <div></div>
+  <AppSection>
+    <div class="text-center">
+      <h2>Instagram @Kidsrus</h2>
+    </div>
+    <div class="images gap-3">
+      <v-img
+        v-for="(image, index) in state.images"
+        :key="index"
+        :src="image"
+      ></v-img>
+    </div>
+  </AppSection>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
 
-<style lang="scss" scoped></style>
+const state = reactive({
+  images: [
+    "family-1.jpg",
+    "family-2.jpg",
+    "family-3.jpg",
+    "family-4.jpg",
+    "family-5.jpg",
+  ],
+});
+</script>
+
+<style lang="scss" scoped>
+.images {
+  display: grid;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+}
+</style>
