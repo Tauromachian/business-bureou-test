dc4c179672fdf78a398337bd06896cc992d8f5de
Jose Garcia <jogarcia@tuta.io>
2023-05-24 20:21:52 -0400

    Import fonts from Google

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,50 @@
 export default defineNuxtConfig({
   modules: ["@invictus.codes/nuxt-vuetify"],
+  app: {
+    head: {
+      title: "Jose Garcia | portfolio",
+      htmlAttrs: {
+        lang: "en",
+      },
+
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Kids e-commerce",
+        },
+
+        { property: "og:title", content: "Kids R Us" },
+        { property: "og:site_name", content: "Kids R Us" },
+        {
+          property: "og:description",
+          content: "Kids e-commerce",
+        },
+        { property: "og:type", content: "website" },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content: "logo.png",
+        },
+        { property: "og:image:alt", content: "Image of the logo" },
+        { property: "og:image:size", content: "300" },
+        { property: "og:locale", content: "en_US" },
+        { property: "og:copyright", content: "Kids store" },
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "logo.png" },
+        { rel: "preconnect", href: "https://fonts.googleapis.com/" },
+        { rel: "preconnect", href: "https://fonts.gstatic.com/" },
+        { rel: "dns-prefetch", href: "https://fonts.googleapis.com/" },
+        { rel: "dns-prefetch", href: "https://fonts.gstatic.com/" },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Francois+One&family=Gilda+Display&family=Quicksand:wght@400;500&display=swap",
+        },
+      ],
+    },
+  },
+
   vuetify: {
     /* vuetify options */
     vuetifyOptions: {
