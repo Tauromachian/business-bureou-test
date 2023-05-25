89755bb28028de537590897818f6995c6e5b7180
Jose Garcia <jogarcia@tuta.io>
2023-05-25 10:09:48 -0400

    Add banner styles

--- components/TheBanner.vue
+++ components/TheBanner.vue
@@ -1,7 +1,16 @@
 <template>
-  <div></div>
+  <p class="banner">
+    20% off all kidswear + free shipping when you spend 60$ or more
+  </p>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.banner {
+  background-color: rgb(var(--v-theme-accent));
+  text-align: center;
+  color: white;
+  padding: 0.2em 0;
+}
+</style>
