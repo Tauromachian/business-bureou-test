7e1e9a77b0d4e72867c4d5b454172344db8fa372
Jose Garcia <jogarcia@tuta.io>
2023-05-25 10:45:24 -0400

    Change layout

--- components/HomeCategories.vue
+++ components/HomeCategories.vue
@@ -1,12 +1,14 @@
 <template>
-  <VCard flat class="border-dotted">
-    <VRow class="text-center">
-      <VCol cols="3" v-for="item in state.items" :key="item.text">
-        <VImg :src="item.image"></VImg>
-        <p>{{ item.text }}</p>
-      </VCol>
-    </VRow>
-  </VCard>
+  <div class="d-flex justify-center">
+    <VCard flat class="border-dotted">
+      <VCardText class="d-flex mx-8 justify-center text-center gap-15">
+        <div v-for="(item, index) in state.items" :key="`image-${index}`">
+          <VImg :src="item.image"></VImg>
+          <h5 class="mt-4">{{ item.text }}</h5>
+        </div>
+      </VCardText>
+    </VCard>
+  </div>
 </template>
 
 <script setup>
@@ -14,10 +16,10 @@ import { reactive } from "vue";
 
 const state = reactive({
   items: [
-    { image: "image-url-1", text: "Some Text 1" },
-    { image: "image-url-2", text: "Some Text 2" },
-    { image: "image-url-3", text: "Some Text 3" },
-    { image: "image-url-4", text: "Some Text 4" },
+    { image: "baby.png", text: "Some Text 1" },
+    { image: "girl.png", text: "Some Text 2" },
+    { image: "boy.png", text: "Some Text 3" },
+    { image: "toys.png", text: "Some Text 4" },
   ],
 });
 </script>
@@ -25,5 +27,7 @@ const state = reactive({
 <style scoped>
 .border-dotted {
   border: 2px dotted #000000;
+  border-color: rgb(223, 221, 218);
+  width: fit-content;
 }
 </style>
