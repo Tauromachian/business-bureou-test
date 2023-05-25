8bcd6de00d72b2a9008982a6c46b2d4b372921c6
Jose Garcia <jogarcia@tuta.io>
2023-05-25 16:33:27 -0400

    Improve styles

--- components/HomeCategories.vue
+++ components/HomeCategories.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="d-flex justify-center">
-    <VCard flat class="border-dotted">
-      <VCardText class="d-flex mx-8 justify-center text-center gap-15">
-        <div v-for="(item, index) in state.items" :key="`image-${index}`">
-          <VImg :src="item.image"></VImg>
-          <h5 class="mt-4">{{ item.text }}</h5>
-        </div>
+    <VCard flat class="w-100 border-dotted">
+      <VCardText class="d-flex mx-8 justify-center text-center">
+        <VRow class="gap-15 mx-14 py-8">
+          <VCol v-for="(item, index) in state.items" :key="`image-${index}`">
+            <VImg :src="item.image"></VImg>
+            <h5 class="mt-4">{{ item.text }}</h5>
+          </VCol>
+        </VRow>
       </VCardText>
     </VCard>
   </div>
