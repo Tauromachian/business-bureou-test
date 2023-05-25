1d4e4d67bd15eca48f9f49fc68082bc4d424b44f
Jose Garcia <jogarcia@tuta.io>
2023-05-25 16:26:39 -0400

    Make hero

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -1,14 +1,36 @@
 <template>
-  <VRow>
-    <VCol md="8">
-      <VImg></VImg>
-    </VCol>
-    <VCol md="4">
-      <VImg></VImg>
-    </VCol>
-  </VRow>
+  <AppSection>
+    <div class="gap-15 images">
+      <VImg class="h-fit col-span-4" src="slider-1.jpg"></VImg>
+      <div class="col-span-2 text-center">
+        <VImg class="h-fit" src="girls-power.jpg"></VImg>
+        <h2>Girl Power</h2>
+        <p>For a colorful summer</p>
+      </div>
+    </div>
+  </AppSection>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.images {
+  display: grid;
+  grid-template-columns: repeat(6, minmax(0, 1fr));
+}
+
+.col-span-4 {
+  grid-column: span 4;
+}
+.col-span-2 {
+  grid-column: span 2;
+}
+
+.girls-power {
+  height: fit-content;
+}
+
+.h-fit {
+  height: fit-content;
+}
+</style>
