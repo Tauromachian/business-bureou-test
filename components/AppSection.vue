4817bd98dc68583ff9a7c6ea125df81b1265d1f9
Jose Garcia <jogarcia@tuta.io>
2023-05-25 11:17:29 -0400

    Create AppSection component

---
+++ components/AppSection.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <slot></slot>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+section {
+  margin-top: 6em;
+  margin-bottom: 6em;
+}
+</style>
