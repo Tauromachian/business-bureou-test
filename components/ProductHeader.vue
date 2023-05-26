0aaa37a58d351262b443c9e98bfa515558e130d0
Jose Garcia <jogarcia@tuta.io>
2023-05-25 20:48:35 -0400

    Make basic ProductHeader component

---
+++ components/ProductHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center my-14">
+    <h1>Blue Blouse</h1>
+    <div class="d-flex justify-center">
+      <VBreadcrumbs
+        :items="['Home', 'For Girls', 'Blue Blouse']"
+        divider="."
+        active-class="active-link"
+      ></VBreadcrumbs>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+h1 {
+  font-family: var(--accent-font-family);
+}
+
+.active-link {
+  background-color: rgb(var(--v-theme-accent));
+}
+</style>
