c0507796a3a0559a762fda9901d1aae4fbcc013d
Jose Garcia <jogarcia@tuta.io>
2023-05-26 16:32:10 -0400

    Make basic AppHeader component

---
+++ components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="d-flex flex-column align-center">
+    <h2>
+      <slot></slot>
+    </h2>
+    <span class="d-flex justify-center w-25">
+      <VImg src="/separator.png"></VImg>
+    </span>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+div {
+  width: fit-content;
+}
+</style>
