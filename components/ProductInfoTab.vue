e0eed291b82f1c8e7a9ef6f0cae14e59b0b89f64
Jose Garcia <jogarcia@tuta.io>
2023-05-25 20:54:57 -0400

    Make basic info tab content

--- components/ProductInfoTab.vue
+++ components/ProductInfoTab.vue
@@ -1,7 +1,44 @@
 <template>
-  <div></div>
+  <div>
+    <v-tabs v-model="activeTab" align-tabs="center" class="mb-10">
+      <v-tab value="description">Description</v-tab>
+      <v-tab value="additional-info">Additional Information</v-tab>
+      <v-tab value="reviews">Reviews</v-tab>
+    </v-tabs>
+
+    <VWindow v-model="activeTab">
+      <VWindowItem value="description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+          temporibus fugit rem soluta assumenda? Magni beatae natus consectetur
+          facilis amet veritatis vitae quibusdam magnam. Harum deleniti cumque
+          adipisci ducimus dolore!
+        </p>
+      </VWindowItem>
+      <VWindowItem value="additional-info">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+          temporibus fugit rem soluta assumenda? Magni beatae natus consectetur
+          facilis amet veritatis vitae quibusdam magnam. Harum deleniti cumque
+          adipisci ducimus dolore!
+        </p>
+      </VWindowItem>
+      <VWindowItem value="reviews">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+          temporibus fugit rem soluta assumenda? Magni beatae natus consectetur
+          facilis amet veritatis vitae quibusdam magnam. Harum deleniti cumque
+          adipisci ducimus dolore!
+        </p>
+      </VWindowItem>
+    </VWindow>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const activeTab = ref(null);
+</script>
 
 <style lang="scss" scoped></style>
