29f04c9cd49ec9d44f68520bd37a155131f376ea
Jose Garcia <jogarcia@tuta.io>
2023-05-28 14:51:16 -0400

    Use v-model of HomeCategories

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
   <AppContainer>
     <HomeHero></HomeHero>
-    <HomeCategories></HomeCategories>
+    <HomeCategories v-model="state.selectedCategories"></HomeCategories>
     <HomeMainContent></HomeMainContent>
     <HomeInstagram></HomeInstagram>
   </AppContainer>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+
+const state = reactive({
+  selectedCategories: [],
+});
+</script>
 
 <style lang="scss" scoped></style>
