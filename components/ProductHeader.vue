0e584009a6eca40c93ead75aea7351ae43f1de5d
Jose Garcia <jogarcia@tuta.io>
2023-05-27 21:04:27 -0400

    Ref/ Remove unnecessary tag

--- components/ProductHeader.vue
+++ components/ProductHeader.vue
@@ -1,14 +1,12 @@
 <template>
-  <AppCard class="w-100">
-    <div class="text-center">
-      <h1>Blue Blouse</h1>
-      <div class="d-flex justify-center">
-        <VBreadcrumbs
-          :items="['Home', 'For Girls', 'Blue Blouse']"
-          divider="."
-          active-class="active-link"
-        ></VBreadcrumbs>
-      </div>
+  <AppCard class="w-100 text-center">
+    <h1>Blue Blouse</h1>
+    <div class="d-flex justify-center">
+      <VBreadcrumbs
+        :items="['Home', 'For Girls', 'Blue Blouse']"
+        divider="."
+        active-class="active-link"
+      ></VBreadcrumbs>
     </div>
   </AppCard>
 </template>
