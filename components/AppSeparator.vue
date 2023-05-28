b4cd39aeb40520debdae661453bc9dfe89267f09
Jose Garcia <jogarcia@tuta.io>
2023-05-28 11:41:04 -0400

    Make basic app separator

---
+++ components/AppSeparator.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="d-flex justify-center">
+    <img v-if="active" src="/separator.png" />
+  </span>
+</template>
+
+<script setup>
+defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+span {
+  height: 12px;
+  width: 60%;
+}
+
+img {
+  width: 100%;
+}
+</style>
