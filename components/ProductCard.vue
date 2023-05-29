9743f1d2132fd628a318e57a6576ad14713568c7
Jose Garcia <jogarcia@tuta.io>
2023-05-29 17:58:21 -0400

    Create productCard component

---
+++ components/ProductCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <VCard flat class="text-center rounded-0">
+    <VImg :src="product.image" aspect-ratio="1"></VImg>
+    <VCardSubtitle class="mt-4">
+      <h4 class="category text-accent">
+        {{ product.category }}
+      </h4>
+    </VCardSubtitle>
+    <VCardTitle
+      ><h3>
+        {{ product.name }}
+      </h3>
+    </VCardTitle>
+    <VCardText>
+      <h4>
+        {{ product.price }}
+      </h4>
+    </VCardText>
+    <VRating
+      v-if="showRating"
+      :model-value="product.rating"
+      density="compact"
+      color="accent"
+      readonly
+    ></VRating>
+  </VCard>
+</template>
+
+<script setup>
+defineProps({
+  product: {
+    type: Object,
+    default: () => ({}),
+  },
+  showRating: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
