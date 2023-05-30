f383003a6426a48418eb09892e77d55217669b59
Jose Garcia <jogarcia@tuta.io>
2023-05-29 21:15:50 -0400

    Add links to breadcrumbs

--- components/ProductHeader.vue
+++ components/ProductHeader.vue
@@ -3,7 +3,7 @@
     <h1>{{ product.name }}</h1>
     <div class="d-flex justify-center">
       <VBreadcrumbs
-        :items="['Home', product.categoryText, product.name]"
+        :items="items"
         divider=":"
         active-class="active-link"
       ></VBreadcrumbs>
@@ -12,10 +12,20 @@
 </template>
 
 <script setup>
+import { computed } from "vue";
 import { useProductStore } from "@/stores/product";
+
 const productStore = useProductStore();
 
 const product = productStore.product;
+
+const items = computed(() => {
+  return [
+    { title: "Home", disabled: false, href: "/" },
+    { title: product.categoryText, disabled: false, href: "/" },
+    { title: product.name, disabled: true },
+  ];
+});
 </script>
 
 <style lang="scss" scoped>
